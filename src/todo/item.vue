ae3d518ab21496aaae50c8e2c29558f6c2183a6c
Authur <1738238581@qq.com>
2020-01-15 20:28:34 +0800

    20200115，配置@babel/core

--- src/todo/item.vue
+++ src/todo/item.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="['todo-item',todo.completed?'completed': '']">
     <input type="checkbox" class="toggle" v-model="todo.completed" />
-    <label>{{todo.completed}}</label>
+    <label>{{todo.content}}</label>
     <button class="destory" @click="deleteTodo"></button>
   </div>
 </template>
@@ -15,9 +15,11 @@ export default {
     }
   },
   methods: {
-    deleteTodo() {}
+    deleteTodo() {
+      this.$emit('del',this.todo.id)
+    }
   }
-};
+}
 </script>
 
 <style lang="stylus" scoped>
